b9f0aa8ef4591dac45f0f5890cfe54cadb4ed6f9
Harrf Akbar <harrf_akbar@yahoo.com>
2021-08-26 22:12:18 -0700

    finsihed work section

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Header from './Header'
 import About from './About'
 import Project from './projects/Project'
+import Work from "./Work";
 
 const App = () => {
 
@@ -11,6 +12,7 @@ const App = () => {
         <Header />
         <About />
         <Project />
+        <Work />
       
      </main>
   )
---
+++ src/components/Work.js
@@ -0,0 +1,133 @@
+import React from 'react';
+
+const Work = () => {
+
+    return (
+       <section id="section-work" className="section-work">
+          <div className="work__wrapper">
+             <div className="work__content">
+                <div className="center-text-margin">
+                   <h2 className="heading-secondary">
+                      Work Experience
+                      <div className="underline"></div>
+                   </h2>
+                </div>
+                <div>
+                   <h4>
+                      <span className="work__title">Manager</span>, Salad Makers{" "}
+                      <span className="work__date">February 2009 - January 2021 </span>
+                   </h4>
+                   <ul>
+                      <li>
+                         Manage a million-dollar restaurant comprised of salads and sandwiches{" "}
+                      </li>
+                      <li>
+                         Monitor a team of 10-18 employees to help provide excellent customer
+                         service{" "}
+                      </li>
+                      <li>
+                         Provide over 400+ customers oer day with friendly and positive service
+                         ensuring customers will leave satisfied
+                      </li>
+                      <li>
+                         Handle marketing and promotiosn for the restaurant through schools,
+                         hospitals, festivals, gyms and city events, resulting in new catering
+                         service for schools and hospitals, which have increased the revenue by 33%
+                      </li>
+                      <li>
+                         Designed a loyalty program to reward revisiting customers resulting in an
+                         89% increase in revenue and increased the average of a customer returning
+                         to 3 visits a week
+                      </li>
+                      <li>
+                         Trained over 100+ employees in customer service skills, food industry
+                         knowledge, and in food health and safety regulations{" "}
+                      </li>
+                   </ul>
+                </div>
+                <div>
+                   <h4>
+                      <span className="work__title">Universal Banker</span>, US Bank
+                      <span className="work__date">April 2019 - January 2021 </span>
+                   </h4>
+                   <ul>
+                      <li>
+                         Provide over 100+ customers per day with excellent service ensuring they
+                         receive the help they needed
+                      </li>
+                      <li>
+                         Maintain a personal 100 percent customer satisfaction by customers reviews
+                         through the whole year{" "}
+                      </li>
+                      <li>
+                         Help customers with their financial needs able to identify what exactly
+                         they need and provide with the best possible options financially{" "}
+                      </li>
+                      <li>
+                         Developed skills and knowledge in bank teller functions, loans, HELOC, and
+                         business accounts{" "}
+                      </li>
+                      <li>
+                         Audited the branch cash flow, banker’s cashbox, audit documents that were
+                         required to be up to dated or needed to meet new compliances{" "}
+                      </li>
+                   </ul>
+                </div>
+                <div>
+                   <h4>
+                      <span className="work__title">Sales Associate</span>, Express{" "}
+                      <span className="work__date">November 2011 - January 2015 </span>
+                   </h4>
+                   <ul>
+                      <li>
+                         Provided over 100+ customer per day with excellent customer service
+                         ensuring customers will leave satisfied{" "}
+                      </li>
+                      <li>
+                         Rearranged the store every other month and promotions understanding that
+                         presentation is a key factor in sales
+                      </li>
+                      <li>
+                         Promoted sales by listening what the customer wants, giving them fashion
+                         advice, and providing them with great service that they can always rely on
+                         when making decisions
+                      </li>
+                   </ul>
+                </div>
+             </div>
+          </div>
+          <div className="section-ed">
+             <div className="work__wrapper">
+                <div className="work__content">
+                   <div className="center-text-margin">
+                      <h2 className="heading-secondary">
+                         Education
+                         <div className="underline"></div>
+                      </h2>
+                   </div>
+                   <div>
+                      <h4>
+                         <span className="work__title">Flatiron School</span>, California
+                         <span className="work__date">April 2021</span>
+                      </h4>
+                      <ul>
+                         <li>Full Stack Web Development, Ruby on Rails and JavaScript program</li>
+                      </ul>
+                   </div>
+                   <div>
+                      <h4>
+                         <span className="work__title">California State University of EastBay</span>
+                         , California
+                         <span className="work__date">June 2014</span>
+                      </h4>
+                      <ul>
+                         <li>Bachelor of Science in Criminal Justice</li>
+                      </ul>
+                   </div>
+                </div>
+             </div>
+          </div>
+       </section>
+    )
+}
+export default Work;
